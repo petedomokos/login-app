10fe6f942ebb10ab24490fbfde9333d9bdea9454
Peter Domokos <peterdomokos@Peters-MacBook-Air.local>
2021-02-05 12:47:44 +0000

    added success handling. If login is successful, it is assumed that a secure jwt token is sent as data property. this is stored to session storage, and also in a userJwt state object which triggers a re-render whihc redirects to the main system. This could be an integrated system, or an external link. Excat impl of this step still to be determined.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 //MUI
 import { makeStyles } from '@material-ui/core/styles';
 //helpers
@@ -35,32 +36,55 @@ export default function Login(props) {
       error:''
   }
   const [status, setStatus] = useState(initStatus);
+  const [userJwt, setUserJwt] = useState('');
   //destructuring
   const { credentials, attemptingLogin, error } = status;
   const { username, password } = credentials;
+  //user input
   const handleChange = (e) => {
     const { name, value } = e.target;
      setStatus(status => ({...status, credentials:{...status.credentials, [name]: value } }));
   }
+  //user submit
   const handleSubmit = async (e) => {
       e.preventDefault();
       if (username && password) {
           setStatus(status => ({...status, attemptingLogin:true}))
           //api call
           const data = await attemptLogin(status.credentials);
-          console.log('data', data)
           if(data.error){
-            console.log('failure', data)
             //add error and reset attemptingLogin and password
             const updatedCredentials = {...credentials, password:''}
             setStatus(status => ({...status, attemptingLogin:false, error:data.error, credentials:updatedCredentials}));
           }
           else{
-            console.log('success', data)
+            //reset attemptingLogin
+            setStatus(status => ({...status, attemptingLogin:false}));
+            //if no error, then data will contain a JWT token (assumed back-end implemtation)
+            //store user jwt token in session storage 
+            //note alternative options: (a) could use redux store but no need,
+            // or (b) could use Route to get to Home instead, and pass user to Home as props -but only if
+            //this component was to be integrated with app, rather than standalone login
+            if (typeof window !== "undefined"){
+              sessionStorage.setItem('jwt', JSON.stringify(data.jwt))
+              //update token in component state to trigger re-render and hence Redirect
+              setUserJwt(data.jwt)
+            }
           }
       }else{
         alert('Please provide a username and password.')
       }
+  }
+
+  if(userJwt){
+      //user is authenticated
+      //if integrated with app, then redirect to Home page or referrer (defaults to '/')
+      //if standalone, then consider options -> server can store jwt token so it is accessible from other apps (ie for single sign-on)
+      //home page, and other pages, can access token via session storage
+      //temp mock home page in place of external link
+      return(<MockHome/>)
+      //for integrated app
+      //return(<Redirect to={referrerUrl})
   }
 
   return (
@@ -88,6 +112,15 @@ export default function Login(props) {
 }
 
 Login.propTypes = {
+  referrerUrl:PropTypes.string
 }
 Login.defaultProps = {
+  referrerUrl:'/'
 }
+
+const MockHome = () => {
+  return (
+    <div>Home page</div>
+    )
+}
+
